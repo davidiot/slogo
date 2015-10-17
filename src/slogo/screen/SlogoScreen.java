39bfaad36de40f15de66005b249386e87677ec7d
David Zhou <xdavidzhou@gmail.com>
2015-10-17 18:47:52 -0400

    put comments back in

--- src/slogo/screen/SlogoScreen.java
+++ src/slogo/screen/SlogoScreen.java
@@ -65,6 +65,8 @@ public class SlogoScreen extends AbstractScreen {
 		if (console.hasInput()) {
 			String command = console.getInput();
 			myEngineController.sendToInterpreter(command);
+			map.getCharacter(0).move(1000);
+			map.getCharacter(0).rotateCharacter(Integer.parseInt(command));
 			h.add(command);
 		}
 		map.updateCharacters();
