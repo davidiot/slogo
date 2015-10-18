9d7dd5c0cf39d7a51b56f0d2dfcec41f0cae4e0a
Daniel Pak <daniel1996@gmail.com>
2015-10-17 21:07:20 -0400

    Added html formatted help page

--- src/slogo/screen/HelpScreen.java
+++ src/slogo/screen/HelpScreen.java
@@ -1,7 +1,11 @@
 package slogo.screen;
 
+import javax.print.DocFlavor.URL;
+
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 public class HelpScreen extends AbstractWindowScreen {
 
@@ -18,9 +22,16 @@ public class HelpScreen extends AbstractWindowScreen {
 
 	private GridPane makeTitle() {
 		GridPane title = new GridPane();
-		Text temp = createText(myResources.getString("helpTitle"),
-				Integer.parseInt(myResources.getString("smallTitle")));
-		title.add(temp, 0, 0);
+		WebView browser = new WebView();
+		WebEngine webEngine = browser.getEngine();
+		java.net.URL resourceURL = Thread.currentThread().getContextClassLoader().getResource("helpPage/SLogoHelp.html");
+		String test = "file:///" + resourceURL.getFile();
+		System.out.println(test);
+		webEngine.load(test);
+		title.getChildren().add(browser);
+		//Text temp = createText(myResources.getString("helpTitle"),
+		//		Integer.parseInt(myResources.getString("smallTitle")));
+		//title.add(temp, 0, 0);
 		return title;
 	}
 
