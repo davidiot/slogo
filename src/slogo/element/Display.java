ca6d124054d48eafc1c166637a84eb829f4fdffe
David Zhou <xdavidzhou@gmail.com>
2015-10-18 05:29:36 -0400

    merged uhoh into master

--- src/slogo/element/Display.java
+++ src/slogo/element/Display.java
@@ -42,7 +42,7 @@ public class Display extends AbstractElement {
 						+ 2 * Double.parseDouble(slogoResources.getString("characterCenterX")),
 				Double.parseDouble(slogoResources.getString("mapHeight"))
 						+ 2 * Double.parseDouble(slogoResources.getString("characterCenterY")),
-				Color.RED);
+				Color.TRANSPARENT);
 		map = new Rectangle(Integer.parseInt(slogoResources.getString("mapWidth")),
 				Integer.parseInt(slogoResources.getString("mapHeight")), Color.WHITE);
 		test = new Canvas(Integer.parseInt(slogoResources.getString("mapWidth")),
